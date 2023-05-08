32aab982274391193f6fd5e625d806b2e8839e58
Oleg <unxxxt@gmail.com>
2023-05-08 20:08:43 +0300

    Add permission logic in :app
    Android 8, 9 requires READ_EXTERNAL_STORAGE permission
    Android 10 requires READ_EXTERNAL_STORAGE permission and requestLegacyExternalStorage="true" flag
    Android 11 requires MANAGE_EXTERNAL_STORAGE permission

--- app/src/main/java/com/example/vkfileviewer/MainActivity.kt
+++ app/src/main/java/com/example/vkfileviewer/MainActivity.kt
@@ -1,11 +1,58 @@
 package com.example.vkfileviewer
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
+import android.os.Environment
+import android.provider.Settings
+import androidx.annotation.RequiresApi
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import android.Manifest
+import androidx.core.app.ActivityCompat
+
+const val REQUEST_CODE_READ_EXTERNAL_STORAGE = 1
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        requestStoragePermissions()
+    }
+
+    private fun requestStoragePermissions() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            if (!Environment.isExternalStorageManager()) {
+                requestManageExternalStoragePermission()
+            }
+        } else {
+            if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED) {
+                requestReadExternalStoragePermission()
+            }
+        }
+    }
+
+    @RequiresApi(Build.VERSION_CODES.R)
+    private fun requestManageExternalStoragePermission() {
+        val uri = Uri.parse("package:${BuildConfig.APPLICATION_ID}")
+        startActivity(
+            Intent(
+                Settings.ACTION_MANAGE_APP_ALL_FILES_ACCESS_PERMISSION,
+                uri
+            )
+        )
+    }
+
+    private fun requestReadExternalStoragePermission() {
+        ActivityCompat.requestPermissions(
+            this,
+            arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE),
+            REQUEST_CODE_READ_EXTERNAL_STORAGE
+        )
     }
 }
