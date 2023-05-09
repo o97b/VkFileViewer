50bcdf8e0b68f00604b327144d6f74da93f0bb1b
Oleg <unxxxt@gmail.com>
2023-05-09 03:17:31 +0300

    Fix start navigation logic in :app
    Move request permission functions from activity to fragment.

--- app/src/main/java/com/example/vkfileviewer/MainActivity.kt
+++ app/src/main/java/com/example/vkfileviewer/MainActivity.kt
@@ -1,18 +1,8 @@
 package com.example.vkfileviewer
 
-import android.content.Intent
-import android.content.pm.PackageManager
-import android.net.Uri
-import android.os.Build
 import android.os.Bundle
-import android.os.Environment
-import android.provider.Settings
-import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.content.ContextCompat
-import android.Manifest
-import androidx.core.app.ActivityCompat
-import androidx.navigation.fragment.NavHostFragment
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -21,58 +11,4 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
     }
 
-    override fun onResume() {
-        super.onResume()
-        checkPermissions()
-    }
-
-    private fun checkPermissions() {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
-            if (Environment.isExternalStorageManager()) {
-                navigateToFileList()
-            } else {
-                requestManageExternalStoragePermission()
-            }
-        } else {
-            if (ContextCompat.checkSelfPermission(
-                    this,
-                    Manifest.permission.READ_EXTERNAL_STORAGE
-                ) == PackageManager.PERMISSION_GRANTED) {
-                navigateToFileList()
-            } else {
-                requestReadExternalStoragePermission()
-            }
-        }
-    }
-
-    private fun navigateToFileList() {
-        val navHostFragment = supportFragmentManager
-            .findFragmentById(R.id.fragment_container_view_tag) as NavHostFragment
-        val navController = navHostFragment.navController
-        navController.navigate(R.id.fileListFragment)
-    }
-
-
-    @RequiresApi(Build.VERSION_CODES.R)
-    private fun requestManageExternalStoragePermission() {
-        val uri = Uri.parse("package:${BuildConfig.APPLICATION_ID}")
-        startActivity(
-            Intent(
-                Settings.ACTION_MANAGE_APP_ALL_FILES_ACCESS_PERMISSION,
-                uri
-            )
-        )
-    }
-
-    private fun requestReadExternalStoragePermission() {
-        ActivityCompat.requestPermissions(
-            this,
-            arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE),
-            REQUEST_CODE_READ_EXTERNAL_STORAGE
-        )
-    }
-
-    companion object {
-        const val REQUEST_CODE_READ_EXTERNAL_STORAGE = 1
-    }
 }
--- app/src/main/java/com/example/vkfileviewer/ui/StartFragment.kt
+++ app/src/main/java/com/example/vkfileviewer/ui/StartFragment.kt
@@ -1,7 +1,71 @@
 package com.example.vkfileviewer.ui
 
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
+import android.os.Build
+import android.os.Environment
+import android.provider.Settings
+import androidx.annotation.RequiresApi
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.example.vkfileviewer.BuildConfig
 import com.example.vkfileviewer.R
 
 class StartFragment: Fragment(R.layout.start_fragment) {
+
+    override fun onResume() {
+        super.onResume()
+        checkPermissions()
+    }
+
+    private fun checkPermissions() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            if (Environment.isExternalStorageManager()) {
+                navigateToFileList()
+            } else {
+                requestManageExternalStoragePermission()
+            }
+        } else {
+            if (ContextCompat.checkSelfPermission(
+                    requireActivity(),
+                    Manifest.permission.READ_EXTERNAL_STORAGE
+                ) == PackageManager.PERMISSION_GRANTED) {
+                navigateToFileList()
+            } else {
+                requestReadExternalStoragePermission()
+            }
+        }
+    }
+
+    private fun navigateToFileList() {
+        val navController = findNavController()
+        navController.navigate(R.id.fileListFragment)
+    }
+
+    @RequiresApi(Build.VERSION_CODES.R)
+    private fun requestManageExternalStoragePermission() {
+        val uri = Uri.parse("package:${BuildConfig.APPLICATION_ID}")
+        startActivity(
+            Intent(
+                Settings.ACTION_MANAGE_APP_ALL_FILES_ACCESS_PERMISSION,
+                uri
+            )
+        )
+    }
+
+    private fun requestReadExternalStoragePermission() {
+        ActivityCompat.requestPermissions(
+            requireActivity(),
+            arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE),
+            REQUEST_CODE_READ_EXTERNAL_STORAGE
+        )
+    }
+
+    companion object {
+        const val REQUEST_CODE_READ_EXTERNAL_STORAGE = 1
+    }
 }
